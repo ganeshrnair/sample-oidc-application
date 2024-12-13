63db393f333608377da53dd682936e28f5720ed0
Ganesh Ramachandran Nair <ganeshrnair@gmail.com>
2024-12-13 08:38:18 +0530

    v1

--- adminportal/src/main/java/com/example/adminportal/AdminportalController.java
+++ adminportal/src/main/java/com/example/adminportal/AdminportalController.java
@@ -1,15 +1,36 @@
 package com.example.adminportal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Controller
 public class AdminportalController {
+
+    @Value("${limitsapi.url}")
+    private String limitsapiUrl;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
 
     @GetMapping("/")
     public String index(@AuthenticationPrincipal OidcUser user, Model model) {
@@ -34,11 +55,39 @@ public class AdminportalController {
                 }
             }
         }
+
+        model.addAttribute("dailyLimit", callLimitsApi());
         return "home";  // This is the login page or landing page
     }
 
-    @GetMapping(value = "/ssl-test")
-    public String inbound(){
-        return "Inbound TLS is working!!";
+    private String callLimitsApi() {
+
+        // Extract the authentication token from the SecurityContext
+        OAuth2AuthenticationToken authenticationToken = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+
+        // Get the access token from the OAuth2AuthorizedClientService
+        OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(
+                authenticationToken.getAuthorizedClientRegistrationId(),
+                authenticationToken.getName()
+        );
+
+        // Get access token from the authorized client
+        String accessToken = authorizedClient.getAccessToken().getTokenValue();
+
+        // Prepare the headers with Bearer token
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken);
+
+        // Build the request
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        String response = "Unable to get data from LimitsApi";
+        try {
+            response = restTemplate.exchange(limitsapiUrl, HttpMethod.GET, request, String.class).getBody();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response;
     }
 }
