21d20aca005bdf0b1059bc1d809184934d814e05
Ganesh Ramachandran Nair <ganeshrnair@gmail.com>
2024-12-17 06:14:30 +0530

    api

--- limitsapi/src/main/java/com/example/adminportal/LimitsapiController.java
+++ limitsapi/src/main/java/com/example/adminportal/LimitsapiController.java
@@ -1,5 +1,7 @@
 package com.example.adminportal;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,13 @@ import java.util.List;
 class LimitsapiController {
 
     @GetMapping("/dailylimit")
-    public String getDailyLimitForCustomers() {
-        return "This is a secure endpoint and requires authentication.";
+    public String getDailyLimitForCustomers(@AuthenticationPrincipal Jwt jwt) {
+        String username = jwt.getClaim("preferred_username");
+        String userId = jwt.getSubject(); // The "sub" claim
+        String scopes = jwt.getClaim("scp");
+
+        return String.format("UserId: %s, Username: %s, Scopes: %s", userId, username, scopes);
+
     }
 
 }
